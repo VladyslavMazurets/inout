2ee44218492f6b146578a31eb4f72cddbb8b25cd
vmazurets <vladmazurec@gmail.com>
2024-11-02 21:46:58 +0100

    feat: added eslint and prettier rules

--- resources/js/app/Pages/Landing/Index.vue
+++ resources/js/app/Pages/Landing/Index.vue
@@ -1,10 +1,10 @@
 <template>
+  <div>
+    <h1 class="w-full text-2xl font-bold text-blue-800">Hello word!</h1>
     <div>
-        <h1 class="w-full text-blue-800 text-2xl font-bold">Hello word!</h1>
-        <div>
-            <!-- <span class="text-5xl text-green-500"> Test text New test </span> -->
-        </div>
+      <!-- <span class="text-5xl text-green-500"> Test text New test </span> -->
     </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
